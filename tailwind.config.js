c249b4c3b97336831833364789f9ce6c5b865a4b
Tobias Peters <peterstobias2302@gmail.com>
2021-10-24 13:10:50 +0200

    Added dropzone

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@ module.exports = {
   purge: { enabled: false, content: ["./src/**/*.ejs"] },
   darkMode: false,
   theme: {
-    fontFamily: {
-      sans: ["Poppins", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto"],
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto"],
+      },
+      colors: {
+        primary: {
+          light: "#c7d5f8",
+          DEFAULT: "#7494ec",
+        },
+        secondary: "#5874c6",
+        neutral: {
+          light: "#f8fbff",
+          DEFAULT: "#f2f7ff",
+        },
+        gray: {
+          light: "#8b98bb",
+          DEFAULT: "#69779B",
+        },
+      },
     },
   },
   variants: {
