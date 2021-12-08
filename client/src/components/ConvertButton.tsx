6f7d0f78aa89513e0c367934db383b7c0f9205c1
Tobias Peters <peterstobias2302@gmail.com>
2021-12-08 22:40:17 +0100

    Prepared File Upload

--- client/src/components/ConvertButton.tsx
+++ client/src/components/ConvertButton.tsx
@@ -1,12 +1,30 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
+import { IFile, useFiles } from "../context/file/FileProvider";
+import { useErrorMessage } from "../context/message/ErrorMessageProvider";
 
 type ConvertButtonProps = {
   text: string;
 };
 
 export default function ConvertButton({ text }: ConvertButtonProps) {
+  const { addedFiles } = useFiles();
+  const { addMessage, removeMessage } = useErrorMessage();
+
+  function handleClick() {
+    removeMessage();
+
+    if (!checkForTemplates(addedFiles)) return addMessage("Nicht alle Dateien haben eine Vorlage.");
+  }
+
+  function checkForTemplates(files: IFile[]) {
+    return files.every((f) => f.template !== "");
+  }
+
   return (
-    <button className="group flex justify-between items-center py-5 px-8 bg-thenex-blue text-white font-bold text-lg transition-all hover:bg-opacity-90">
+    <button
+      className="group flex justify-between items-center py-5 px-8 bg-thenex-blue text-white font-bold text-lg transition-all hover:bg-opacity-90"
+      onClick={handleClick}
+    >
       <span className="mr-8">{text}</span>
       <ArrowRightIcon className="w-5 h-5 opacity-70 transition-transform transform group-hover:translate-x-1" />
     </button>
--- client/src/components/ErrorMessageItem.tsx
+++ client/src/components/ErrorMessageItem.tsx
@@ -11,11 +11,11 @@ export default function ErrorMessageItem({ text }: ErrorMessageItemProps) {
   return (
     <div className="flex items-center mt-10 -mb-6 px-4 py-2 bg-red-100 text-red-700 border border-red-400">
       <ExclamationCircleIcon className="flex-shrink-0 w-5 h-5" />
-      <p className="ml-4">
+      <p className="mx-4">
         <span className="inline-block mr-1 font-medium">Fehler beim Upload!</span>
         {text}
       </p>
-      <XIcon className="flex-shrink-0 w-5 h-5 ml-2 cursor-pointer" onClick={removeMessage} />
+      <XIcon className="ml-auto flex-shrink-0 w-5 h-5 cursor-pointer" onClick={removeMessage} />
     </div>
   );
 }
