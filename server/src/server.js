7c33fb67d19a33be89157ae42d511de1737adad9
Tobias Peters <peterstobias2302@gmail.com>
2021-12-31 15:07:50 +0100

    Added _redirects file for Netlify

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const config = require("./config/config");
@@ -5,7 +6,7 @@ const fileRouter = require("./routes/file.routes");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
