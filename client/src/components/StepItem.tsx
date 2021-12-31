42ae34a14f10fe1545035207b3e21fa56fbd36c9
Tobias Peters <peterstobias2302@gmail.com>
2021-12-31 13:14:32 +0100

    Added descriptions to the three different steps

--- client/src/components/StepItem.tsx
+++ client/src/components/StepItem.tsx
@@ -1,21 +1,17 @@
 type StepItemProps = {
   count: string;
   title: string;
-  description?: string;
+  description: string;
 };
 
-export default function StepItem({ count, title }: StepItemProps) {
+export default function StepItem({ count, title, description }: StepItemProps) {
   return (
     <div className="flex flex-col items-center max-w-xs px-8">
       <div className="flex justify-center items-center w-8 h-8 bg-white text-thenex-blue font-bold border border-solid border-thenex-gray rounded-full">
         {count}
       </div>
       <h3 className="py-2 text-thenex-gray-dark lg:text-lg">{title}</h3>
-      <p className="text-thenex-gray text-center text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex debitis, fugit possimus minus
-        corrupti, deleniti excepturi accusantium quia dolores quaerat maxime adipisci ipsa,
-        repudiandae deserunt quisquam.
-      </p>
+      <p className="text-thenex-gray text-center text-sm">{description}</p>
     </div>
   );
 }
--- client/src/components/StepList.tsx
+++ client/src/components/StepList.tsx
@@ -1,11 +1,17 @@
 import StepItem from "./StepItem";
 
 export default function StepList() {
+  const stepDescriptions = [
+    "Wählen Sie die PDF-Dokumente, die Sie konvertieren möchten, über den Button oder per Drag & Drop aus. Suchen Sie sich dann die passende Vorlage heraus. Zur Verfügung steht eine Auswahl der Thenex-Kunden.",
+    "Klicken Sie auf den Konvertieren-Button. Die PDF-Dokumente werden dann hochgeladen. Im nächsten Schritt wird für Sie eine Excel-Datei erstellt. Der Vorgang kann einige Sekunden dauern.",
+    "Jetzt steht Ihre neue Importdatei für das Topix-System zum Download bereit. Nachdem der Download abgeschlossen wurde, werden die vorher hochgeladenen PDF-Dokumente vom Server entfernt.",
+  ];
+
   return (
     <div className="flex flex-col items-center space-y-12 mt-28 lg:flex-row lg:items-start lg:justify-center lg:space-y-0">
-      <StepItem count="1" title="Dokumente auswählen" />
-      <StepItem count="2" title="Konvertierung starten" />
-      <StepItem count="3" title="Importdatei herunterladen" />
+      <StepItem count="1" title="Dokumente auswählen" description={stepDescriptions[0]} />
+      <StepItem count="2" title="Konvertierung starten" description={stepDescriptions[1]} />
+      <StepItem count="3" title="Importdatei herunterladen" description={stepDescriptions[2]} />
     </div>
   );
 }
